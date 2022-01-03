b3d67626d9c63a3e592e2f424a877ababa38ee41
James Bian <75390860+jamesbian-jb@users.noreply.github.com>
2022-01-03 21:28:39 +0000

    Gave up on my old work, and followed a new way.

--- Sketch.java
+++ Sketch.java
@@ -1,113 +1,141 @@
+
+//import javax.net.ssl.SSLPeerUnverifiedException;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-  public float circleY = 50;
-  public float circleX = 50;
-  int darker1 = 305;
-  int darker2 = 518;
-  int darker3 = 650;
+  float brightY = 500;
+  float brightX = -100;
+  float sunSpeedX = 1;
+  float sunSpeedY = 1;
+
+  float darkY = 500;
+  float darkX = -100;
+  float moonspeedX = 1;
+  float moonspeedY = 1;
+
+  float red = 138;
+  float green = 192;
+  float blue = 230;
+
+  float red2 = 138;
+  float green2 = 192;
+  float blue2 = 230;
+  float counter = 1;
 
   /*
-   * Colours used:
-   * Sky Blue: (140, 237, 250)
-   * Sunset: (253, 151, 20)
-   * Night: (0, 0, 0)
+   * Palette:
+   * Morning: (138, 192, 230)
+   * Sunset: (253, 140, 20)
+   * Night: (10, 10, 10)
    */
 
   public void settings() {
-    size(500, 500);
+    size(400, 400);
   }
 
   public void draw() {
-    int darkconvert1 = 11 * darker1 / 24;
-    int darkconvert2 = 11 * darker2 / 24;
-    int darkconvert3 = 11 * darker3 / 24;
-    boolean moon = false;
 
-    if (moon == false) {
-      if ((darkconvert1 > 37) && (darkconvert2 > 145) && (darkconvert3 > 189)) {
-        darker1 = darker1 - 1;
-        darker2 = darker2 - 1;
-        darker3 = darker3 - 1;
-      }
+    if (counter % 2 == 1) {
+      brightX = brightX + sunSpeedX;
+      double placeholder;
+      placeholder = (0.0025 * (Math.pow(brightX, 2)) + -(brightX) + 150);
+      brightY = (float) placeholder;
+      background(red, green, blue);
 
-      background(darkconvert1, darkconvert2, darkconvert3);
-      fill(249, 240, 100);
-      ellipse(circleX - 100, circleY + 250, 50, 50);
-
-      if (circleY + 200 > 50) {
-        circleY = circleY - 1;
-      }
-
-      if (circleY + 200 == 75) {
-        circleY = circleY - 0;
-      }
-
-      if (circleX > 430) {
-        circleY = circleY + 2;
-
-        if ((450 > circleX) && (circleX > 380)) {
-          if (darkconvert1 < 253) {
-            darker1 = darker1 + 10;
-          }
-          if (darkconvert3 > 20) {
-            darker3 = darker3 - 10;
-          }
-          if (darkconvert2 > 151) {
-            darker2 = darker2 + 10;
-          }
+      if ((brightX >= 10) && (brightX < 200)) {
+        if (red > 50) {
+          red = red - 1;
         }
-
-        if ((circleX > 450) && (circleX < 480)) {
-          if (darkconvert1 < 253) {
-            darker1 = darker1 + 8;
-          }
-          if (darkconvert2 > 151) {
-            darker2 = darker2 + 4;
-          }
-          if (darkconvert3 > 20) {
-            darker3 = darker3 - 2;
-          }
+        if (green > 140) {
+          green = green - 1;
+        }
+        if (blue < 250) {
+          blue = blue + 1;
         }
       }
-      if (circleX > 600) {
-        darker1 = darker1 - 10;
-        darker2 = darker2 - 10;
-        darker3 = darker3 - 10;
-      }
-      circleX = circleX + 1;
 
-      if (circleX > height + 175) {
-        circleX = 0;
-        circleY = 100;
-        moon = true;
-      fill(darkconvert1, darkconvert2, darkconvert3);
-      stroke(darkconvert1, darkconvert2, darkconvert3);
-      ellipse(circleX - 90, circleY + 250, 35, 35);
-      if (circleY + 200 > 50) {
-        circleY = circleY - 1;
+      if ((brightX >= 300) && (brightX < 350)) {
+        if (red < 253) {
+          red = red + 4;
+        }
+        if (green < 140) {
+          green = green - 6;
+        }
+        if (blue > 20) {
+          blue = blue - 5;
+        }
       }
 
-      if (circleY + 200 == 75) {
-        circleY = circleY - 0;
+      if (brightX >= 425) {
+        if (red >= 10) {
+          red = red - 10;
+        }
+        if (green >= 10) {
+          green = green - 10;
+        }
+        if (blue >= 10) {
+          blue = blue - 10;
+        }
       }
-
-      if (circleX > 430) {
-        circleY = circleY + 2;
-        if (circleY + 200 > 50) {
-          circleY = circleY - 1;
-        }
-
-        if (circleY + 200 == 75) {
-          circleY = circleY - 0;
-        }
-
-        if (circleX > 430) {
-          circleY = circleY + 2;
-          }
-        }
+      // reset
+      if (brightX == 500) {
+        counter++;
+        brightX = -75;
+        brightY = 500;
       }
     }
+
+    if (counter % 2 == 0) {
+
+      darkX = darkX + moonspeedX;
+      double coordshift2;
+      coordshift2 = (0.0025 * (Math.pow(darkX, 2)) + -(darkX) + 150);
+      darkY = (float) coordshift2;
+
+      if (darkX <= 400) {
+        red2 = 10;
+        green2 = 10;
+        blue2 = 10;
+
+      }
+      background(red2, green2, blue2);
+
+      if (darkX > 400) {
+        if (red2 < 140) {
+          red2 = red2 + 20;
+        }
+        if (green2 < 237) {
+          green2 = green2 + 20;
+        }
+        if (blue2 < 250) {
+          blue2 = blue2 + 20;
+        }
+      }
+
+      if (darkX == 500) {
+        counter--;
+        darkX = -75;
+        darkY = 500;
+        red = red2;
+        green = green2;
+        blue = blue2;
+
+      }
+    }
+    stroke(255, 255, 255);
+    fill(255, 255, 255);
+    ellipse(darkX, darkY, 50, 50);
+    noStroke();
+    fill(red2, green2, blue2);
+    ellipse(darkX + 10, darkY, 35, 35);
+
+    stroke(249, 240, 100);
+    fill(249, 240, 100);
+    ellipse(brightX, brightY, 50, 50);
+
+    fill(131, 101, 57);
+    stroke(100);
+    rect(0, 300, 400, 300);
   }
 }
